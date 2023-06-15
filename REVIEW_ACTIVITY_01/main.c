a7bcf9ec308d24554d0dd1a0d06fc16099bd4484
Joshua Rodriguez <rodriguezjoshuausc@gmail.com>
2023-06-15 17:12:19 +0800

    First add

---
+++ REVIEW_ACTIVITY_01/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "StaticArray.c"
+#include "DynamicArray.c"
+// include the library you want to test
+
+int main(int argc, char *argv[]) {
+	return 0;
+}
