02e6391a6535bdb7f0e4947e408fc29396d3c7ec
JoshuaCT-coder <114605183+JoshuaCT-coder@users.noreply.github.com>
2023-06-20 13:48:07 +0800

    Update main.c

--- LAB_ACT_3_EMPLOYEE/main.c
+++ LAB_ACT_3_EMPLOYEE/main.c
@@ -10,9 +10,14 @@ int main(){
 	addEmployee(&emplist, newEmployee(1011,"Maria Posada","Female",40.0));
 	addEmployee(&emplist, newEmployee(1012,"Derrick Posada","Male",50.0));
 	addEmployee(&emplist, newEmployee(1013,"Juan Posada","Male",60.0));
-	addEmployee(&emplist, newEmployee(1014,"Jerome Posada","Female",40.0));
+	addEmployee(&emplist, newEmployee(1014,"Carmen Posada","Female",40.0));
+	addEmployee(&emplist, newEmployee(1015,"Barry Posada","Male",60.0));
+	addEmployee(&emplist, newEmployee(1016,"Juanita Posada","Female",40.0));
+	addEmployee(&emplist, newEmployee(1017,"Jann Posada","Female",60.0));
+	addEmployee(&emplist, newEmployee(1018,"Nina Posada","Female",40.0));
 	displayEmployeeList(emplist);
 	List femEmp = getFemaleEmployee(&emplist);
+	printf("\n\t-----------FEMALE-----------\n");
 	displayEmployeeList(femEmp);
 	return 0;
 }
