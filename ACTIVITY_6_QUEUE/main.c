d46f67e5c871fa78d74ec97dca3d497b34bf3728
Joshua Rodriguez <rodriguezjoshuausc@gmail.com>
2023-06-23 23:16:30 +0800

    Changed header files from .c to .h

--- ACTIVITY_6_QUEUE/main.c
+++ ACTIVITY_6_QUEUE/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "Circular.c"
+#include "Circular.h"
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
