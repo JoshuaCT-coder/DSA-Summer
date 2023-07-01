1f24fefc993058cf9fd05794b8a746017efeeb55
Joshua Rodriguez <rodriguezjoshuausc@gmail.com>
2023-07-02 02:15:26 +0800

    Made a menu

--- ACT_9_DICTIONARY/main.c
+++ ACT_9_DICTIONARY/main.c
@@ -10,8 +10,34 @@ int main(int argc, char *argv[]) {
 	int size=20;
 	Dictionary d;
 	initDict(&d,26);
-	populate(&d);
-	visualize(d);
+	String Menu[4]={"Insert","Delete","Visualize","Exit"};
+	int i,choice;
+
+	printf("------MENU------\n");
+	do{
+	for(i=0;i<4; i++){
+		printf("%-10s [%d]\n",Menu[i],i+1);
+	}
+	printf("----------------\n");
+	printf("Enter a choice: ");
+	scanf("%d",&choice);
+		switch(choice){
+			case 0:
+				break;
+			case 1:
+			populate(&d);
+				break;
+			case 2:
+			//removeElem();
+				break;
+			case 3: 
+			visualize(d);
+				break;
+			default:
+			printf("Out of Bounds\n");
+			break;
+		}
+	}while(choice!=4);
 	freeDictionary(&d);
 	
 		return 0;
